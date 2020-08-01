b02cd4bd2efcf10989b35e4fbe6b70ae151a3fce
deborrrrrah <dajtpbl@gmail.com>
2020-08-01 16:31:41 +0700

    Add swapcase

--- week-1/playful_bot.rb
+++ week-1/playful_bot.rb
@@ -1,5 +1,5 @@
 print "Hi, I'm Playful Bot. What is your name?\n> "
 name = gets.chomp
-print "Nice name, #{name.reverse}. Where do you come from?\n> "
+print "#{name.reverse.swapcase}! Very nice to meet you! Where do you come from?\n> "
 country = gets.chomp
 puts "#{country} is a #{country.length}-letter word. #{country.length*13} is your lucky number!"
