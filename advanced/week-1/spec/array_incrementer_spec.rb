b4087bf94f7fa9a161092bd0d2fb57e5dd77777e
deborrrrrah <dajtpbl@gmail.com>
2021-02-02 21:49:14 +0700

    [Debo] Refactor test code

--- advanced/week-1/spec/array_incrementer_spec.rb
+++ advanced/week-1/spec/array_incrementer_spec.rb
@@ -1,33 +1,36 @@
 require '../class/array_incrementer.rb'
 
-RSpec.describe '#increment' do
-  it 'return [1] when input empty array' do
-    incrementer = ArrayIncrementer.new
-    result = incrementer.increment([])
-    expect(result).to eq([1])
+describe '#increment' do
+  before (:all) do
+    @incrementer = ArrayIncrementer.new
+  end 
+
+  context 'when input empty array' do
+    it 'return [1] when input empty array' do
+      result = @incrementer.increment([])
+      expect(result).to eq([1])
+    end
   end
 
-  it 'return [1] when input [0]' do
-    incrementer = ArrayIncrementer.new
-    result = incrementer.increment([0])
-    expect(result).to eq([1])
-  end
-
-  it 'return [2] when input [1]' do
-    incrementer = ArrayIncrementer.new
-    result = incrementer.increment([1])
-    expect(result).to eq([2])
-  end
-
-  it 'return [3] when input [2]' do
-    incrementer = ArrayIncrementer.new
-    result = incrementer.increment([2])
-    expect(result).to eq([3])
-  end
-
-  it 'return [1, 0] when input [9]' do
-    incrementer = ArrayIncrementer.new
-    result = incrementer.increment([9])
-    expect(result).to eq([1, 0])
+  context 'when input single element array' do
+    it 'return [1] when input [0]' do
+      result = @incrementer.increment([0])
+      expect(result).to eq([1])
+    end
+  
+    it 'return [2] when input [1]' do
+      result = @incrementer.increment([1])
+      expect(result).to eq([2])
+    end
+  
+    it 'return [3] when input [2]' do
+      result = @incrementer.increment([2])
+      expect(result).to eq([3])
+    end
+  
+    it 'return [1, 0] when input [9]' do
+      result = @incrementer.increment([9])
+      expect(result).to eq([1, 0])
+    end
   end
 end
