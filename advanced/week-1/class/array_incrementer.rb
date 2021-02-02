d15088dbe6318ec8c622425a46c469ac10d6ef18
deborrrrrah <dajtpbl@gmail.com>
2021-02-02 20:49:12 +0700

    [Debo] Refactor test return [1] when input empty array and [0]]

--- advanced/week-1/class/array_incrementer.rb
+++ advanced/week-1/class/array_incrementer.rb
@@ -1,9 +1,5 @@
 class ArrayIncrementer
   def increment(input)
-    if input.empty?
-      [1]
-    elsif (input == [0])
-      [1]
-    end
+    [1]
   end
 end
--- advanced/week-1/spec/array_incrementer_spec.rb
+++ advanced/week-1/spec/array_incrementer_spec.rb
@@ -1,13 +1,13 @@
 require '../class/array_incrementer.rb'
 
-RSpec.describe "#increment" do
-  it "return [1] when input empty array" do
+RSpec.describe '#increment' do
+  it 'return [1] when input empty array' do
     incrementer = ArrayIncrementer.new
     result = incrementer.increment([])
     expect(result).to eq([1])
   end
 
-  it "return [1] when input [0]" do
+  it 'return [1] when input [0]' do
     incrementer = ArrayIncrementer.new
     result = incrementer.increment([0])
     expect(result).to eq([1])
