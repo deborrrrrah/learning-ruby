dbbbb237b65c0f26370f520e3ea030495a6ac254
deborrrrrah <dajtpbl@gmail.com>
2021-02-04 21:03:56 +0700

    [Debo] Refactor test code for message_operation

--- advanced/week-1/spec/message_operation_spec.rb
+++ advanced/week-1/spec/message_operation_spec.rb
@@ -2,27 +2,26 @@ require '../class/message_operation.rb'
 
 RSpec.describe 'MessageOperation' do
   describe '#send_email' do
+    before(:each) do
+      @sender = 'person_1'
+      @recipient = '@person_2'
+      @email = 'some text'
+      @owner = 'person_1' 
+    end
+
     it 'return successfully sent' do
       email_utility = double
       allow(email_utility).to receive(:send_email).and_return(true)
-      
-      sender = 'person_1'
-      recipient = 'person_2'
-      email = 'some text'
-      
-      result = MessageOperation.new(email_utility).send_email(sender, recipient, email) 
+      expect(email_utility).to receive(:send_email).with(@sender, @recipient, @email)
+      result = MessageOperation.new(email_utility).send_email(@sender, @recipient, @email)
       expect(result).to eq('Email successfully sent')
     end
 
     it 'return failure when sending email' do
       email_utility = double
       allow(email_utility).to receive(:send_email).and_return(false)
-      
-      sender = 'person_1'
-      recipient = 'person_2'
-      email = 'some text'
-      
-      result = MessageOperation.new(email_utility).send_email(sender, recipient, email) 
+      expect(email_utility).to receive(:send_email).with(@sender, @recipient, @email)
+      result = MessageOperation.new(email_utility).send_email(@sender, @recipient, @email) 
       expect(result).to eq('Failure when sending email')
     end
   end
@@ -31,20 +30,16 @@ RSpec.describe 'MessageOperation' do
     it 'return empty list when no emails retrieved' do
       email_utility = double
       allow(email_utility).to receive(:retrieve_emails).and_return([])
-      
-      owner = 'person_1'
-      
-      result = MessageOperation.new(email_utility).retrieve_emails(owner) 
+      expect(email_utility).to receive(:retrieve_emails).with(@owner)
+      result = MessageOperation.new(email_utility).retrieve_emails(@owner) 
       expect(result).to eq([])
     end
 
     it 'return ["email text 1", "email text 2"] when some emails retrieved' do
       email_utility = double
       allow(email_utility).to receive(:retrieve_emails).and_return(['email text 1', 'email text 2'])
-      
-      owner = 'person_1'
-      
-      result = MessageOperation.new(email_utility).retrieve_emails(owner) 
+      expect(email_utility).to receive(:retrieve_emails).with(@owner)
+      result = MessageOperation.new(email_utility).retrieve_emails(@owner) 
       expect(result).to eq(['email text 1', 'email text 2'])
     end
   end
